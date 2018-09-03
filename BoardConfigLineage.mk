fc88b07d2792afd9d840c9f06d703e170fe8a527
Julian Fischer <myst3rion9@gmail.com>
2018-09-03 21:32:16 +0200

    dragon: Boot permissive for now

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,6 +5,8 @@ TARGET_KERNEL_SOURCE := kernel/google/dragon
 TARGET_KERNEL_CONFIG := lineageos_dragon_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.fit
 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
 # Do not build libhealthd.lineage
 WITH_LINEAGE_CHARGER := false
 
