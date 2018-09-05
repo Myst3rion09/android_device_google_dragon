c204a40a7feebc8bb0f6a7d2ab28e49687ae81fc
Julian Fischer <myst3rion9@gmail.com>
2018-09-05 18:14:35 +0200

    dragon: Fix Build

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,6 +1,5 @@
 # Inline kernel
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/google/dragon
 TARGET_KERNEL_CONFIG := lineageos_dragon_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.fit
