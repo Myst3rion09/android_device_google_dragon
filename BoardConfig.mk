7139a847a9596d6677cbfb89f5b76c5639744f32
followmsi <followmsi@gmail.com>
2019-03-05 11:37:25 +0100

    dragon: Update vendor security patch level -> 2019-03-05

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,6 +112,9 @@ ART_USE_HSPACE_COMPACT=true
 # let charger mode enter suspend
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# Security Patch Level
+VENDOR_SECURITY_PATCH := 2019-03-05
+
 BOARD_SEPOLICY_DIRS += device/google/dragon/sepolicy
 
 # add firmware update to the updater binary
